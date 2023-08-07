2e91c0fb75703af44ea2654adbe1e810cc7e7a06
Piyush Singh <1806397@kiit.ac.in>
2023-08-07 15:53:40 +0530

    Optimizing Context

--- src/PostProvider.js
+++ src/PostProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 //1. Create a new context
 const PostContext = createContext();
@@ -31,19 +31,16 @@ function PostProvided({ children }) {
   function handleClearPosts() {
     setPosts([]);
   }
-  return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
-  );
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchedPosts, searchQuery]);
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
 }
 function usePosts() {
   const context = useContext(PostContext);
